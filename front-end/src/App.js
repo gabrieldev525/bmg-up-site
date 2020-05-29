4b6fb0eb45964802ec6b365fc36bd2327d403a4c
garciamendes <garciaprog0101@gmail.com>
2020-05-29 15:50:13 -0300

    Readjustment and removal of unnecessary files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,39 +1,7 @@
-import React from 'react';
-
-
+import React from 'react'
 
 function App() {
-
-  return (
-    <>
-    <h1> Wello World!</h1>
-    
-    <form>
-      <label>
-        Nome:
-    <input type="text" name="name" required />
-      </label>   
-         
-      <label>
-        Email:
-    <input type="Email" name="Email" required />
-      </label>
-
-      <label>
-        Telefone:
-    <input type="number" name="Telefone" required />
-      </label>
-    <input type="submit" value="Enviar" />
-  </form>
-
-    </>
-
-
-
-
-  );
+  return <h1>Hello World</h1>
 }
 
-
-
-export default App;
+export default App
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 
 ReactDOM.render(
@@ -8,6 +8,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-
+)
